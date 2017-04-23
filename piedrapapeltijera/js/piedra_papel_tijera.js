201bc8c5380fe482f6f816a58b90fe65214ebad2
Emmanuel Flores <emmanuelflores2010@gmail.com>
2017-04-23 15:25:08 -0300

    Algunos cambios

--- piedrapapeltijera/js/piedra_papel_tijera.js
+++ piedrapapeltijera/js/piedra_papel_tijera.js
@@ -13,45 +13,67 @@ let selec_maquina = 0;
 let ptsjugador = 0;
 let ptsmaquina = 0;
 
+function selec_random(max) {
+  return Math.floor(Math.random()*max + 1);
+}
+
 function seleccion_de_la_maquina() {
   if (!checkbox.checked) {
-    selec_maquina = Math.floor(Math.random()*3 + 1);
+    selec_maquina = selec_random(3);
     imagencomp.src = "imagenes/d"+selec_maquina+".png"
+  } /*else {  // Aleatorio en piedra y papel //
+    selec_maquina = selec_random(2);
+    imagencomp.src = "imagenes/d"+selec_maquina+".png"
+  }*/
+}
+
+function felicitar() {
+  if ((ptsjugador >= 3) && ((ptsjugador % 3) == 0)) {
+    alert("Felicitaciones has ganado "+ptsjugador+" veces");
   }
 }
 
 function ganador(selec_jugador) {
   if (((selec_jugador == piedra) && (selec_maquina == tijera)) || ((selec_jugador == tijera) && (selec_maquina == piedra))) {
     if (selec_jugador == piedra) {
-      resultado.src = "imagenes/ganaste.jpg"
+      resultado.src = "imagenes/ganaste.png"
+      document.body.style.backgroundColor = "#D0F5A9";
       ptsjugador += 1;
+      felicitar();
     } else {
-      resultado.src = "imagenes/perdiste.jpg"
+      resultado.src = "imagenes/perdiste.png"
+      document.body.style.backgroundColor = "##F5A9A9";
       ptsmaquina += 1;
     }
   } else {
     if (selec_jugador > selec_maquina) {
-      resultado.src = "imagenes/ganaste.jpg"
+      resultado.src = "imagenes/ganaste.png"
+      document.body.style.backgroundColor = "#D0F5A9";
       ptsjugador += 1;
+      felicitar();
     } else {
       if (selec_jugador < selec_maquina) {
-        resultado.src = "imagenes/perdiste.jpg"
+        resultado.src = "imagenes/perdiste.png"
+        document.body.style.backgroundColor = "#F5A9A9";
         ptsmaquina += 1;
       }else{
         resultado.src = "imagenes/nada.png"
+        document.body.style.backgroundColor = "#A9F5F2";
       }
     }
   }
+  ganadasjugador.innerHTML = ptsjugador;
+  ganadasmaquina.innerHTML = ptsmaquina;
 }
 
 function va_ganando() {
   if (ptsjugador > ptsmaquina) {
-    ganadasjugador.style.color = "green";
-    ganadasmaquina.style.color = "red";
+    ganadasjugador.style.color = "#45c71a";
+    ganadasmaquina.style.color = "#bc1430";
   } else {
     if (ptsjugador < ptsmaquina) {
-      ganadasjugador.style.color = "red";
-      ganadasmaquina.style.color = "green";
+      ganadasjugador.style.color = "#bc1430";
+      ganadasmaquina.style.color = "#45c71a";
     } else {
       ganadasjugador.style.color = "blue";
       ganadasmaquina.style.color = "blue";
@@ -62,8 +84,6 @@ function va_ganando() {
 function selec_jugador(selec_jugador) {
   imagenjug.src = "imagenes/i"+selec_jugador+".png"
   ganador(selec_jugador);
-  ganadasjugador.innerHTML = ptsjugador;
-  ganadasmaquina.innerHTML = ptsmaquina;
   va_ganando();
 }
 
